c5fae6d0dc33dd952a0d33742a25fee60b7280c9
Daiane Bushey <daianebushey@gmail.com>
2017-12-28 21:27:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(phrase)
   if phrase !== phrase.upcase
     return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
 end
 
 # Whatever you say to grandma, she should respond with
